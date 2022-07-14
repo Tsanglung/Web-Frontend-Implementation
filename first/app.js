7b8f6efd96768ae8e9ad1689b8319a071efb9112
Tsanglung <101389446+Tsanglung@users.noreply.github.com>
2022-07-14 21:31:05 +0800

    Add files via upload

---
+++ first/app.js
@@ -0,0 +1,17 @@
+'use strict';/*偵錯訊息*/ 
+
+const switcher = document.querySelector('.btn');/*取得按鈕元素的參考*/
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
